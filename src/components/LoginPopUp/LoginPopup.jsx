1cb69caf165eda254b0c8c2f15713d0795cd695e
jawadi-hiba <hibajawadi88@gmail.com>
2024-07-03 14:51:51 +0200

    first commit

---
+++ src/components/LoginPopUp/LoginPopup.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./loginpopup.css";
+import { assets } from "../../assets/assets";
+
+const LoginPopup = () => {
+  const [currState, setCurrState] = useState("Sign Up");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currState === "login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="your name" required />
+          )}
+          <input type="email" placeholder="your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condtion">
+          <input type="checkbox" required />
+          <p> By continuing ,i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState==="Login" ? (
+          <p>
+            Create a new account?<span onClick={()=>setCurrState("Sign Up")}>click here</span>
+          </p>
+        ) : (
+          <p>
+            All ready have an account?<span onClick={()=>setCurrState("Login")}>login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './home.css'
+import Header from '../../components/Header/Header'
+import  ExploreMenu from'../../components/ExploreMenu/ExploreMenu.jsx'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.jsx'
+const Home = () => {
+  const[category,setCategory] = useState("All")
+  return (
+    <div>
+        <Header/>
+       < ExploreMenu category={category} setCategory={setCategory} />
+       <FoodDisplay category={category} />
+    </div>
+  )
+}
+
+export default Home
